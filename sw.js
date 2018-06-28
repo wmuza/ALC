4116ac1e1716290dfd055ceff9baa08b982bfe6b
wmuza <wmuza@meticulosity.co.za>
2018-06-28 18:17:05 +0200

    Updating Service Worker

--- sw.js
+++ sw.js
@@ -6,8 +6,8 @@
 */
 
 // registering service worker cache 
-var appCacheName = 'ALC-static-v1';
-var appCacheAssets = [
+var appCacheName = 'currency-converter--static-v12';
+var urlsToCache = [
 	  '/index.html',
 	  '/css/app.css',
 	  '/js/app.js',
@@ -24,41 +24,54 @@ var appCacheAssets = [
 
 
 // on install state
-self.addEventListener('install', function(event){
-	event.waitUntil(
-		caches.open(appCacheName).then(function(cache){
-			return cache.addAll(appCacheAssets);
-		})
-	);
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(appCacheName)
+      .then((cache) => cache.addAll(urlsToCache))
+  )
 });
 
 // on activate state
-self.addEventListener('activate', function(event){
-	event.waitUntil(
-		caches.keys().then(function(cacheNames){
-			return Promise.all(
-				cacheNames.filter(function(cacheName){
-					return cacheName.startsWith('wnes-') && cacheName !== appCacheName;
-				}).map(function(cacheName){
-					return caches.delete(cacheName);
-				})
-			);
-		})
-	);
-});
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.filter((cacheName) => {
+          return cacheName.startsWith('currency-converter-') &&
+            cacheName !== appCacheName;
+        }).map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+})
 
 // on fetch state
-self.addEventListener('fetch', function(event){
-	// event.respondWith('hello');
-	// console.log('hello');
-	event.respondWith(
-		caches.match(event.request).then(function(response){
-			if(response){
-				return response;
-			}
-			return fetch(event.request);
-		})
-	);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          (response) => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            
+            return response;
+          }
+        )
+      })
+  )
 });
 
 // on message
