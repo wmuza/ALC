03e55060b0c92fd08af4b3fc2fa9852e9ef1e9ff
wmuza <wmuza@meticulosity.co.za>
2018-06-28 16:44:52 +0200

    register services worker

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
- 
+ /**
+* Author: Wilbert Muza
+* Version: 1.0.0
+* Signature: wmuza
+*/
+
+'use strict';
+
+$(document).ready(function (){
+	fetchAllCurrencies();
+});
+
+/*
+|------------------------------------------
+| SERVICE WORKER SECTION
+|------------------------------------------
+*/
+
+// register services worker
+if(navigator.serviceWorker){
+	// register the services worker
+	registerServiceWorker();
+
+	// listen for controller change
+	navigator.serviceWorker.addEventListener('controllerchange', function (){
+		window.location.reload();
+	});
+
+}else{
+	console.log('browser does not support Services Worker !');
+}
